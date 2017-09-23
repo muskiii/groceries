624f7130070b195338be7bd3aaf2b494545f79aa
aspen <jmf@grayshirts.com.ar>
2017-09-23 13:43:09 -0300

    3.2: Navigating screens

---
+++ app/views/login/login.js
@@ -0,0 +1,15 @@
+
+var frameModule = require("ui/frame");
+
+exports.loaded = function() {
+    console.log("hello");
+};
+
+exports.signIn = function() {
+    alert("Signing in");
+};
+
+exports.register = function() {
+    var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
+};
