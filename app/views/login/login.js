eecbddee2f1d6cb151bcc5f49e0bd4de89983c43
aspen <jmf@grayshirts.com.ar>
2017-09-23 16:01:14 -0300

    chapter 3 done

--- app/views/login/login.js
+++ app/views/login/login.js
@@ -1,15 +1,26 @@
-
 var frameModule = require("ui/frame");
 
-exports.loaded = function() {
-    console.log("hello");
+var page;
+var email;
+
+var observableModule = require("data/observable");
+
+var user = new observableModule.fromObject({
+    email: "user@domain.com",
+    password: "password"
+});
+exports.loaded = function(args) {
+    page = args.object;
+    page.bindingContext = user;
 };
 
 exports.signIn = function() {
-    alert("Signing in");
+    email = page.getViewById("email");
+    console.log(email.text);
 };
 
 exports.register = function() {
     var topmost = frameModule.topmost();
     topmost.navigate("views/register/register");
 };
+
